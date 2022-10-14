5cd169ee074beca92de36634a4d74a27492a99b5
cHacHaem <113141138+cHacHaem@users.noreply.github.com>
2022-10-14 16:57:05 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var spr2X = 80;
 var spr2Y = 80;
 alert("Click Screen Before Using Control Keys")
 function setup() {
-  createCanvas(windowWidth, windowHeight-65)
+  createCanvas(windowWidth, windowHeight-100)
   //getting video ready:
   face = createCapture(VIDEO)
   face.hide()
@@ -58,5 +58,5 @@ function keyPressed() {
 }
 
 function windowResized() {
-  createCanvas(windowWidth, windowHeight-65)
+  createCanvas(windowWidth, windowHeight-100)
 }
