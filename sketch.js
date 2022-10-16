76900088ec4f038083d571a0d6052021a03b3c2a
cHacHaem <113141138+cHacHaem@users.noreply.github.com>
2022-10-16 07:59:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,10 +25,7 @@ function draw() {
   rect(sprX, sprY, 30, 50)
   line(sprX-10, sprY+30, sprX, sprY)
   line(sprX+30, sprY, sprX+40, sprY+30)
-  fill("rgb(0,30,128)")
-  rect(spr2X, spr2Y, 30, 50)
-  line(spr2X-10, spr2Y+30, spr2X, spr2Y)
-  line(spr2X+30, spr2Y, spr2X+40, spr2Y+30)
+  
   
   }
 
