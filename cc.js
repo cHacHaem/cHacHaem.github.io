b758e727cd268877d5930b52405cdc1394aab6d6
cHacHaem <113141138+cHacHaem@users.noreply.github.com>
2022-12-15 18:13:15 -0500

    Create cc.js

---
+++ cc.js
@@ -0,0 +1,30 @@
+    
+    if(keyIsPressed) {
+        //down
+        if(keyCode === 40) {
+            spriteY = spriteY+1;   
+        }
+        // left
+        if(keyCode === 37) {
+            spriteX = spriteX-1;   
+        }
+        // right
+        if(keyCode === 39) {
+            spriteX = spriteX+1;   
+        }
+        //up
+        if(keyCode === 38) {
+            spriteY = spriteY-1;   
+        }
+        
+    }
+    
+
+
+
+
+
+
+
+    
+}
