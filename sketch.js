95e289a890a62c5f47408d2d3eceb471def16e64
cHacHaem <113141138+cHacHaem@users.noreply.github.com>
2023-11-04 11:00:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,36 @@
-    ob1.setAttribute("position", {x: random(-2, 2), y: 12, z: -5})
-    ob2.setAttribute("dynamic-body", "mass", 0)
-    ob2.setAttribute("position", {x: random(-2, 2), y: 12, z: -7}) 
+    }
+function draw() {
+  if(keyIsPressed && keyCode == 82) {
+    if(gameOver) {
+     ob1Place() 
+     ob2Place()
       score = 0
-    gameOver = false
+      overText.setAttribute("position", "x", -200)
+    gameOver = false  
     } else {
       ob1.setAttribute("dynamic-body", "mass", 2)
+      
     }
-    ob1.setAttribute("dynamic-body", "mass", 0)
-    ob1.setAttribute("position", {x: random(-2, 2), y: 12, z: -5})
-    ob2.setAttribute("dynamic-body", "mass", 0)
-    ob2.setAttribute("position", {x: random(-2, 2), y: 12, z: -7})
     print("r")
   }
   if(ob1.getAttribute("position", "z").z > 5) {
-    ob1.setAttribute("dynamic-body", "mass", 0)
-    ob1.setAttribute("position", {x: random(-2, 2), y: 12, z: -5})
+    ob1Place()
     print("bye")
     ob2.setAttribute("dynamic-body", "mass", 2)
     score++
     scoreText.setAttribute("text", "value", "score: " + score)
   }
   if(ob2.getAttribute("position", "z").z > 5) {
-    ob2.setAttribute("dynamic-body", "mass", 0)
-    ob2.setAttribute("position", {x: random(-2, 2), y: 12, z: -7})
+    ob2Place()
     print("bye")
     ob1.setAttribute("dynamic-body", "mass", 2)
      score++
     scoreText.setAttribute("text", "value", "score: " + score)
   }
-  if(keyIsPressed && keyCode == LEFT_ARROW && player.getAttribute("position", "X").x > -2 || mouseIsPressed && mouseX < width/2 && player.getAttribute("position", "X").x > -2) {
+  if(keyIsPressed && keyCode == LEFT_ARROW && player.getAttribute("position", "X").x > -gWidth || mouseIsPressed && mouseX < width/2 && player.getAttribute("position", "X").x > -gWidth) {
     player.setAttribute("position", {x: player.getAttribute("position", "X").x - 0.05, y: 1, z: 0});
   } 
-  if(keyIsPressed && keyCode == RIGHT_ARROW && player.getAttribute("position", "X").x < 2 || mouseIsPressed && mouseX > width/2 && player.getAttribute("position", "X").x < 2) {
+  if(keyIsPressed && keyCode == RIGHT_ARROW && player.getAttribute("position", "X").x < gWidth || mouseIsPressed && mouseX > width/2 && player.getAttribute("position", "X").x < gWidth) {
     player.setAttribute("position", {x: player.getAttribute("position", "X").x + 0.05, y: 1, z: 0});
   }
 
