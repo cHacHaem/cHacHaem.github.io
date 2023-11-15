92d284e2911fdd42fcb19715e1a3ed1b09cf7d65
cHacHaem <113141138+cHacHaem@users.noreply.github.com>
2023-11-15 17:54:51 -0500

    Update other.js

--- other.js
+++ other.js
@@ -15,15 +15,19 @@ mouseTrap.setAttribute("position", {x: random(-25, 25), y: 0, z: random(-25, 25)
 }
 function draw() {
   if(keyIsPressed && keyCode == UP_ARROW) {
+    mouse.setAttribute("rotation", "y", -90);
     mouse.setAttribute("position", {z: mouse.getAttribute("position", "z").z-0.1, x: mouse.getAttribute("position", "x").x});
   }  
     if(keyIsPressed && keyCode == DOWN_ARROW) {
+      mouse.setAttribute("rotation", "y", 90);
     mouse.setAttribute("position", {z: mouse.getAttribute("position", "z").z+0.1, x: mouse.getAttribute("position", "x").x});
   }  
   if(keyIsPressed && keyCode == LEFT_ARROW) {
     mouse.setAttribute("position", {x: mouse.getAttribute("position", "x").x-0.1, z: mouse.getAttribute("position", "z").z});
+    mouse.setAttribute("rotation", "y", 0);
   }  
   if(keyIsPressed && keyCode == RIGHT_ARROW) {
+    mouse.setAttribute("rotation", "y", -180);
     mouse.setAttribute("position", {x: mouse.getAttribute("position", "x").x+0.1, z: mouse.getAttribute("position", "z").z});
   }
 }
